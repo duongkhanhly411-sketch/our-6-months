8613e1623aee0049f497ed7148c724e81524ccf2
duongkhanhly411-sketch <duongkhanhly411@gmail.com>
2026-02-07 23:56:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,15 @@ const startDate = new Date("2025-08-18T00:00:00");
 function updateTimer() {
     const now = new Date();
     const diff = now - startDate;
-
     const days = Math.floor(diff / (1000 * 60 * 60 * 24));
     const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
     const mins = Math.floor((diff / 1000 / 60) % 60);
     const secs = Math.floor((diff / 1000) % 60);
 
-    const timerDiv = document.getElementById("timer");
-    if (timerDiv) {
-        timerDiv.innerHTML = `Đã đồng hành cùng nhau: ${days} ngày ${hours} giờ ${mins} phút ${secs} giây`;
+    const timer = document.getElementById("timer");
+    if (timer) {
+        timer.innerHTML = `Đã bên nhau: ${days} ngày ${hours} giờ ${mins} phút ${secs} giây`;
     }
 }
-
 setInterval(updateTimer, 1000);
 updateTimer();
